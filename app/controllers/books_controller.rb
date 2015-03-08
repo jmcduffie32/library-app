d5d6914aed7fab02247acf6e7bbe372bcb0da9d7
Jon McDuffie <bobber321988@gmail.com>
2015-03-08 19:35:12 -0400

    Ready to commit to heroku

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,5 @@
 class BooksController < ApplicationController
-	
+	before_action :set_book, only: [:show, :edit, :update, :destroy]
 
 	def index
 		@books = Book.includes(:genre).search(params[:keyword]).filter(params[:filter])
@@ -7,8 +7,54 @@ class BooksController < ApplicationController
 	end
 
 	def show
-		@book = Book.find(params[:id])
+		
 	end
 
-	
+	def new
+		@book = Book.new
+	end
+
+	def edit
+		
+	end
+
+	def create
+		@book = Book.new(book_params)
+		respond_to do |format|
+			if @book.save
+				format.html { redirect_to @book, notice: 'Book was successfully created.' }
+				format.js
+			else
+				format.html {render :new}
+			end
+		end
+
+	end
+
+	def update
+		respond_to do |format|
+			if @book.update(book_params)
+				format.html { redirect_to @book, notice: 'Book was successfully updated.' }
+			else
+				format.html { render :edit }
+			end
+		end
+		
+	end
+
+	def destroy
+		@book.destroy
+		respond_to do |format|
+			format.html { redirect_to books_url, notice: 'Book was successfuly deleted'}
+		end	
+	end
+
+	private
+		def set_book
+			@book = Book.find(params[:id])
+		end
+
+		def book_params
+			params.require(:book).permit(:name, :author, :description, :genre)
+		end
 end
--- app/helpers/books_helper.rb
+++ app/helpers/books_helper.rb
@@ -1,2 +1,6 @@
 module BooksHelper
+	def image_from_ol(isbn)
+		image_tag "http://covers.openlibrary.org/b/isbn/#{isbn}-M.jpg"
+		
+	end
 end
